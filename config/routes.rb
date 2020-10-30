f1f2936ca948d0055702eb24e0da4588962f9d6f
willamy388720 <willamygt@gmail.com>
2020-10-30 16:22:26 -0300

    building endpoint for ratings

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :stores, only: %i[index show]
+      resources :ratings, only: %i[create]
     end
   end
 end
